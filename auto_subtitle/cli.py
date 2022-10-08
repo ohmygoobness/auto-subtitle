b027454e136f41b73240efc757a3f47cda788eb4
ohmygoobness <82442572+ohmygoobness@users.noreply.github.com>
2022-10-09 01:31:47 +0530

    Update cli.py

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -16,53 +16,49 @@ def main():
                         choices=whisper.available_models(), help="name of the Whisper model to use")
     parser.add_argument("--output_dir", "-o", type=str,
                         default=".", help="directory to save the outputs")
-    parser.add_argument("--output_srt", type=str2bool, default=False,
-                        help="whether to output the .srt file along with the video files")
-    parser.add_argument("--srt_only", type=str2bool, default=False,
-                        help="only generate the .srt file and not create overlayed video")
     parser.add_argument("--verbose", type=str2bool, default=False,
-                        help="whether to print out the progress and debug messages")
+                        help="Whether to print out the progress and debug messages")
 
     parser.add_argument("--task", type=str, default="transcribe", choices=[
                         "transcribe", "translate"], help="whether to perform X->X speech recognition ('transcribe') or X->English translation ('translate')")
 
+    parser.add_argument("--only_srt", default=False, action='store_true',
+                        help="only a srt file is generated and not a subtitled video")
+
     args = parser.parse_args().__dict__
     model_name: str = args.pop("model")
     output_dir: str = args.pop("output_dir")
-    output_srt: bool = args.pop("output_srt")
-    srt_only: bool = args.pop("srt_only")
+    only_srt: bool = args.pop("only_srt")
+
     os.makedirs(output_dir, exist_ok=True)
 
     if model_name.endswith(".en"):
-        warnings.warn(
-            f"{model_name} is an English-only model, forcing English detection.")
+        warnings.warn(f"{model_name} is an English-only model, forcing English detection.")
         args["language"] = "en"
 
     model = whisper.load_model(model_name)
     audios = get_audio(args.pop("video"))
     subtitles = get_subtitles(
-        audios, output_srt or srt_only, output_dir, lambda audio_path: model.transcribe(audio_path, **args)
+        audios, lambda audio_path: model.transcribe(
+            audio_path, **args), only_srt, output_dir
     )
-
-    if srt_only:
-        return
-
     # bash command to download a youtube video with `youtube-dl` and save it as `video.mp4`:
     # youtube-dl -f 22 -o video.mp4 https://www.youtube.com/watch?v=QH2-TGUlwu4
 
-    for path, srt_path in subtitles.items():
-        out_path = os.path.join(output_dir, f"{filename(path)}.mp4")
+    if not only_srt:
+        for path, srt_path in subtitles.items():
+            out_path = os.path.join(output_dir, f"{filename(path)}.mp4")
 
-        print(f"Adding subtitles to {filename(path)}...")
+            print(f"Adding subtitles to {filename(path)}...")
 
-        video = ffmpeg.input(path)
-        audio = video.audio
+            video = ffmpeg.input(path)
+            audio = video.audio
 
-        stderr = ffmpeg.concat(
-            video.filter('subtitles', srt_path, force_style="OutlineColour=&H40000000,BorderStyle=3"), audio, v=1, a=1
-        ).output(out_path).run(quiet=True, overwrite_output=True)
+            stderr = ffmpeg.concat(
+                video.filter('subtitles', srt_path, force_style="OutlineColour=&H40000000,BorderStyle=3"), audio, v=1, a=1
+            ).output(out_path).run(quiet=True, overwrite_output=True)
 
-        print(f"Saved subtitled video to {os.path.abspath(out_path)}.")
+            print(f"Saved subtitled video to {os.path.abspath(out_path)}.")
 
 
 def get_audio(paths):
@@ -84,12 +80,16 @@ def get_audio(paths):
     return audio_paths
 
 
-def get_subtitles(audio_paths: list, output_srt: bool, output_dir: str, transcribe: callable):
-    srt_path = output_dir if output_srt else tempfile.gettempdir()
+def get_subtitles(audio_paths: list, transcribe: callable, only_srt: bool, output_dir: str):
+    if only_srt:
+        dir = output_dir
+    else:
+        dir = tempfile.gettempdir()
+
     subtitles_path = {}
 
     for path, audio_path in audio_paths.items():
-        srt_path = os.path.join(srt_path, f"{filename(path)}.srt")
+        srt_path = os.path.join(dir, f"{filename(path)}.srt")
 
         print(
             f"Generating subtitles for {filename(path)}... This might take a while."
@@ -103,6 +103,8 @@ def get_subtitles(audio_paths: list, output_srt: bool, output_dir: str, transcri
             write_srt(result["segments"], file=srt)
 
         subtitles_path[path] = srt_path
+    if only_srt:
+        print(f"Saved subtitle file to {os.path.abspath(srt_path)}.")
 
     return subtitles_path
 
